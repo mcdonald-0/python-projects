e1bbca7a0ef8b9f4473364a37c9e71d667cb04dc
McDonald Otoyo <mcdonaldotoyo44@gmail.com>
2022-07-06 12:59:17 +0100

    Made changes in guessing_game2.0 such that the it takes two options of answers

--- guessing_game2.0.py
+++ guessing_game2.0.py
@@ -9,19 +9,35 @@ print(f'Thank you {username}, You can start the game!')
 
 
 def check_answer(user_answer, answer, alternative_answer):
-    if user_answer.lower() == answer or user_answer.lower() == alternative_answer:
-        score += 1
-        still_guessing = True
+    global score
+    attempt = 0
+    still_guessing = True
+    while still_guessing and attempt < 3:
+        if user_answer.lower() == answer or user_answer.lower() == alternative_answer:
+            print('Correct')
+            score += 1
+            still_guessing = False
+        else:
+            if attempt < 3:
+                user_answer = input('Incorrect, Try again ')
+                attempt += 1
+                
+    if attempt == 3:
+        print(f'The correct answer is {answer}')
+
+def check_username(user_answer, answer):
+    if user_answer == answer:
         print('Correct')
     else:
-        print('Incorrect')
-
+        print('It\'s a shame you can\'t remember your own username')
+            
+# If you want to ask a question, you put the question and call the function just like this
 question1 = input('Can a penguin fly? ')
 check_answer(question1, 'no', 'nah')
 question2 = input('A group of ravens is called? ')
 check_answer(question2, 'a conspiracy', 'conspiracy')
 question3 = input('What\'s your name? ')
-check_answer(question3, username, username)
+check_username(question3, username)
 question4 = input('What is the current time? ')
 check_answer(question4, time.strftime('%H:%M'), time.strftime('%I:%M%p').lower())
 
